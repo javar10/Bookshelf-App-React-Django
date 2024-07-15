3eefa74fd1330979e44c40d31f163554bc0ae06d
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-15 19:54:53 -0400

    updated admin and added to database

--- frontend/src/features/bookSearch/DisplaySearchedBooks.js
+++ frontend/src/features/bookSearch/DisplaySearchedBooks.js
@@ -10,7 +10,7 @@ const DisplaySearchedBooks = ({imageIdArray, searchData}) => {
                             <div key={id} id={id} className='displayBookSearchCard'>
                                 <img src={`https://covers.openlibrary.org/b/olid/${id}-M.jpg`} />
                                 <h4>{searchData.docs[index].title} - <span style={{ fontStyle: 'italic' }}>{searchData.docs[index].first_publish_year}</span></h4>
-                                <p>by {searchData.docs[index].author_name}</p>
+                                <p>by {searchData.docs[index].author_name[0]}</p>
                             </div>
                         )
                     }
