e404ae328c74fc753605ba8d1a5bf4701c6a973b
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-18 09:34:30 -0400

    setting.py update to CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "http://bookshelfapp-frontend.s3-website.us-east-2.amazonaws.com"
 ]
 
 CORS_ALLOWED_ORIGINS = [
